dcfa4baaf5f0639074d22f7c395fad06073ec429
David Božjak <bozjak@gmail.com>
2021-11-30 16:32:55 +0100

    Used AoC2017Day1 as proof of concept (day0)

--- Day0_Setup/Program.cs
+++ Day0_Setup/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Using AoC2017 Day1 as a test run
+
+var parser = new SingleLineStringInputParser<int>(int.TryParse, str => str.ToCharArray().Select(w => w.ToString()).ToArray());
+var input = new InputProvider<int>("Input.txt", parser.GetValue).ToList();
+
+Console.WriteLine($"Part 1: {SumIfEqual(input)}");
+Console.WriteLine($"Part 2: {SumIfEqual(input, input.Count / 2)}");
+
+int SumIfEqual(IList<int> input, int offset = 1)
+{
+    int sum = 0;
+
+    for (int i = 0; i < input.Count; i++)
+    {
+        int indexToMatch = i + offset;
+        indexToMatch = indexToMatch >= input.Count ? indexToMatch - input.Count : indexToMatch;
+
+        if (input[i] == input[indexToMatch])
+            sum += input[indexToMatch];
+    }
+
+    return sum;
+}
