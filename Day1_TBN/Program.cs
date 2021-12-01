a2a46176d7941e28719eb6e685548155c7c899d9
David Božjak <bozjak@gmail.com>
2021-12-01 06:19:45 +0100

    Abstracted to support sliding window of any size

--- Day1_TBN/Program.cs
+++ Day1_TBN/Program.cs
@@ -18,12 +18,22 @@ prevValue = int.MaxValue;
 
 for (int i = 2; i < input.Count; i++)
 {
-    int slidingWindow = input[i - 2] + input[i - 1] + input[i];
-    if (slidingWindow > prevValue)
+    int slidingWindowValue = GetSlidingWindowSum(input, i, 3);
+    if (slidingWindowValue > prevValue)
     {
         numInc++;
     }
-    prevValue = slidingWindow;
+    prevValue = slidingWindowValue;
 }
 
 Console.WriteLine($"Part 2: {numInc}");
+
+int GetSlidingWindowSum(IList<int> input, int poz, int windowLength)
+{
+    int slidingWindow = input[poz - windowLength + 1];
+    for (int i = poz - windowLength + 2; i < (poz + windowLength - 2) && i < input.Count; i++)
+    {
+        slidingWindow += input[i];
+    }
+    return slidingWindow;
+}
