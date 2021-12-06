2439dd2aa3c49377dbe97e638ef34559f4d71e4c
David Božjak <bozjak@gmail.com>
2021-12-06 06:55:16 +0100

    Solved, needs cleaning up

--- Day6_TBN/Program.cs
+++ Day6_TBN/Program.cs
@@ -3,31 +3,62 @@ var input = new InputProvider<int>("Input.txt", parser.GetValue).ToList();
 
 var school = input.Select(w => new Lanterfish(w)).ToList();
 
-int simulateFor = 256;
-for (int day = 0; day < simulateFor; day++)
+//NaiveSimulatePart1();
+
+var ageDict = school.GroupBy(w => w.Age).ToDictionary(w => w.Key, w => (long)w.Count());
+
+for (int i = 8; i >= 0; i--)
 {
-    List<Lanterfish> spawn = new();
-
-    foreach (var fish in school)
-    {
-        var newFish = fish.SimulateDay();
-
-        if (newFish != null)
-        {
-            spawn.Add(newFish);
-        }
-    }
-
-    school.AddRange(spawn);
-    Console.WriteLine($"School size after {day} days: {school.Count}");
+    if (!ageDict.ContainsKey(i))
+        ageDict[i] = 0;
 }
 
-Console.WriteLine($"Part 1: {school.Count} after {simulateFor} days");
-Console.ReadKey();
+int simulateFor = 256;
+for (int day = 1; day <= simulateFor; day++)
+{
+    var multiPlyingGenerationSize = ageDict[0];
+    var spawn = multiPlyingGenerationSize;
+    
+    for (int i = 0; i < 8; i++)
+    {
+        ageDict[i] = ageDict[i + 1];
+    }
+
+    ageDict[6] = ageDict[6] + multiPlyingGenerationSize;
+    ageDict[8] = spawn;
+
+    Console.WriteLine($"School size after {day} days: {ageDict.Sum(w => w.Value)}");
+}
+
+Console.WriteLine($"Part 2: {school.Count} after {simulateFor} days");
+
+void NaiveSimulatePart1()
+{
+    int simulateFor = 80;
+    for (int day = 0; day < simulateFor; day++)
+    {
+        List<Lanterfish> spawn = new();
+
+        foreach (var fish in school)
+        {
+            var newFish = fish.SimulateDay();
+
+            if (newFish != null)
+            {
+                spawn.Add(newFish);
+            }
+        }
+
+        school.AddRange(spawn);
+        Console.WriteLine($"School size after {day} days: {school.Count}");
+    }
+
+    Console.WriteLine($"Part 1: {school.Count} after {simulateFor} days");
+}
 
 class Lanterfish
 {
-    public int Age { private set; get; }
+    public int Age { set; get; }
 
     public Lanterfish(int initialAge)
     {
