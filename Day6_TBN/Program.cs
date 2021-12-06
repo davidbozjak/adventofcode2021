27543865463030a308db75f2a36459dd3c9edada
David Božjak <bozjak@gmail.com>
2021-12-06 06:18:50 +0100

    Added day 6 solution for part 1, probably not optimized enough for part 2

---
+++ Day6_TBN/Program.cs
@@ -0,0 +1,49 @@
+﻿var parser = new SingleLineStringInputParser<int>(int.TryParse, str => str.Split(",", StringSplitOptions.RemoveEmptyEntries));
+var input = new InputProvider<int>("Input.txt", parser.GetValue).ToList();
+
+var school = input.Select(w => new Lanterfish(w)).ToList();
+
+int simulateFor = 256;
+for (int day = 0; day < simulateFor; day++)
+{
+    List<Lanterfish> spawn = new();
+
+    foreach (var fish in school)
+    {
+        var newFish = fish.SimulateDay();
+
+        if (newFish != null)
+        {
+            spawn.Add(newFish);
+        }
+    }
+
+    school.AddRange(spawn);
+    Console.WriteLine($"School size after {day} days: {school.Count}");
+}
+
+Console.WriteLine($"Part 1: {school.Count} after {simulateFor} days");
+Console.ReadKey();
+
+class Lanterfish
+{
+    public int Age { private set; get; }
+
+    public Lanterfish(int initialAge)
+    {
+        Age = initialAge;
+    }
+
+    public Lanterfish? SimulateDay()
+    {
+        this.Age--;
+
+        if (this.Age < 0)
+        {
+            this.Age = 6;
+            return new Lanterfish(8);
+        }
+
+        return null;
+    }
+}
