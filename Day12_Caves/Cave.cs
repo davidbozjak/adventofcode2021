a1f7824c0167597a0d4ae1fed4a44bc0e42ebf44
David Božjak <bozjak@gmail.com>
2021-12-12 07:10:26 +0100

    Added day 12

---
+++ Day12_Caves/Cave.cs
@@ -0,0 +1,23 @@
+﻿class Cave
+{
+    public string Name { get; init; }
+
+    public bool IsBigCave { get; init; }
+
+    private readonly List<Cave> neighbours = new();
+    public IReadOnlyCollection<Cave> Neighbours => this.neighbours.AsReadOnly();
+
+    public Cave(string name)
+    {
+        this.Name = name;
+        this.IsBigCave = name[0] < 'a';
+    }
+
+    public void AddNeighbour(Cave cave)
+    {
+        if (this.neighbours.Contains(cave)) throw new Exception();
+        if (cave == this) throw new Exception();
+
+        this.neighbours.Add(cave);
+    }
+}
---
+++ Day12_Caves/Program.cs
@@ -0,0 +1,82 @@
+﻿var input = new InputProvider<Link?>("Input.txt", ParseLink).Where(w => w != null).Cast<Link>().ToList();
+var factory = new UniqueFactory<string, Cave>(name => new Cave(name));
+
+foreach (var link in input)
+{
+    var startCave = factory.GetOrCreateInstance(link.Start);
+    var endCave = factory.GetOrCreateInstance(link.End);
+
+    startCave.AddNeighbour(endCave);
+    endCave.AddNeighbour(startCave);
+}
+
+var start = factory.AllCreatedInstances.Where(w => w.Name == "start").First();
+var end = factory.AllCreatedInstances.Where(w => w.Name == "end").First();
+
+var allPaths = new List<List<Cave>>();
+Explore(start, end, new List<Cave>(), allPaths, false);
+
+Console.WriteLine($"Part 1: {allPaths.Count}");
+
+var allPathsWithAdvancedRules = new List<List<Cave>>();
+Explore(start, end, new List<Cave>(), allPathsWithAdvancedRules, true);
+
+Console.WriteLine($"Part 2: {allPathsWithAdvancedRules.Count}");
+
+void Explore(Cave current, Cave endNode, List<Cave> path, List<List<Cave>> completedPaths, bool enhancedRules)
+{
+    if (path.Contains(current) && !current.IsBigCave)
+    {
+        if (!enhancedRules) return;
+
+        if (current == start) return;
+        if (current == end) return;
+
+        if (path.Where(w => !w.IsBigCave).GroupBy(w => w).Select(w => w.Count()).Any(w => w > 1)) 
+            return;
+    }
+
+    path = path.ToList(); // make a copy to not ruin backtracking
+    path.Add(current);
+
+    if (current == endNode)
+    {
+        if (!completedPaths.Any(w => ArePathsTheSame(path, w)))
+        {
+            completedPaths.Add(path);
+        }
+
+        return;
+    }
+
+    foreach (var neighbour in current.Neighbours)
+    {
+        Explore(neighbour, endNode, path, completedPaths, enhancedRules);
+    }
+}
+
+static bool ArePathsTheSame(List<Cave> path1, List<Cave> path2)
+{
+    if (path1.Count != path2.Count) return false;
+
+    for (int i = 0; i < path1.Count; i++)
+    {
+        if (path1[i] != path2[i]) return false;
+    }
+
+    return true;
+}
+
+static bool ParseLink(string? input, out Link? value)
+{
+    value = null;
+
+    if (string.IsNullOrWhiteSpace(input)) return false;
+
+    var parts = input.Split('-', StringSplitOptions.RemoveEmptyEntries);
+    value = new Link(parts[0], parts[1]);
+
+    return true;
+}
+
+record Link(string Start, string End);
