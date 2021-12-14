7eff7926aca3873c8252cd22421b6fdabb353970
David Božjak <bozjak@gmail.com>
2021-12-14 08:07:51 +0100

    Added working day14 part 2 solution

--- Day14_TBN/Program.cs
+++ Day14_TBN/Program.cs
@@ -1,83 +1,49 @@
-﻿using System.Text;
-using System.Text.RegularExpressions;
+﻿using System.Text.RegularExpressions;
 
 var input = new InputProvider<InsertionRule?>("Input.txt", GetInsertionRule)
     .Where(w => w != null).Cast<InsertionRule>().ToList();
 
 var count = input.Count;
 
-//string polymer = "NNCB";
 string polymer = "OOBFPNOPBHKCCVHOBCSO";
 
-var insertionFrequencyDict = input.Select(w => w.InsertedChart).GroupBy(w => w).ToDictionary(w => w.Key, w => (long)0);
-var ruleDioct = input.ToDictionary(w => w, w => (long)0);
+var dict = new Dictionary<string, long>();
+var letterFrequency = polymer.ToCharArray().GroupBy(w => w).ToDictionary(w => w.Key, w => (long)w.Count());
 
-for (int step = 0; step < 10; step++)
+for (int i = 1; i < polymer.Length; i++)
 {
-    var builder = new StringBuilder(polymer);
-
-    foreach (var insertionRule in input)
-    {
-        //var indexes = new List<int>();
-        bool hasFoundFirstIndex = false;
-        for (int i = 0; i < builder.Length; i++)
-        {
-            if (builder[i] < 'A' || builder[i] > 'Z') continue;
-
-            //if (indexes.Count == 1)
-            if (hasFoundFirstIndex)
-            {
-                if (builder[i] == insertionRule.SecondChar)
-                {
-                    builder.Insert(i, insertionRule.InsertedChart);
-                    insertionFrequencyDict[insertionRule.InsertedChart]++;
-                    ruleDioct[insertionRule]++;
-                }
-                //indexes.Clear();
-                hasFoundFirstIndex = false;
-            }
-
-            //if (indexes.Count == 0 && builder[i] == insertionRule.FirstChar)
-            if (!hasFoundFirstIndex && builder[i] == insertionRule.FirstChar)
-            {
-                //indexes.Add(i); 
-                hasFoundFirstIndex = true;
-            }
-            else
-            {
-                //indexes.Clear();
-                hasFoundFirstIndex = false;
-            }
-        }
-    }
-
-    polymer = builder.ToString().ToUpper();
-    //Console.WriteLine($"After step {step + 1} length {polymer.Length}: {polymer}");
-    //Console.WriteLine($"After step {step + 1} length {polymer.Length}");
-
-    var letterFrequency = polymer.ToCharArray().GroupBy(w => w).ToDictionary(w => w.Key, w => w.Count());
-    Console.WriteLine($"Step {step + 1} total length: {polymer.Length} predicted next length: {polymer.Length + polymer.Length - 1}");
-
-    //foreach (var pair in insertionFrequencyDict)
-    //{
-    //    Console.WriteLine($"{pair.Key} : {pair.Value}");
-    //    insertionFrequencyDict[pair.Key] = 0;
-    //}
-
-    foreach (var pair in ruleDioct)
-    {
-        Console.WriteLine($"{pair.Key.FirstChar}{pair.Key.FirstChar} -> {pair.Key.InsertedChart} : {pair.Value}");
-        ruleDioct[pair.Key] = 0;
-    }
-
-    //foreach (var pair in letterFrequency)
-    //{
-    //    Console.WriteLine($"{pair.Key} : {pair.Value} frequency: {Math.Round(pair.Value / (double)polymer.Length * 100, 2)}");
-    //}
+    var key = $"{polymer[i - 1]}{polymer[i]}";
+    if (!dict.ContainsKey(key))
+        dict[key] = 1;
+    else dict[key]++;
 }
 
-//var letterFrequency = polymer.ToCharArray().GroupBy(w => w).ToDictionary(w => w.Key, w => w.Count());
-//Console.WriteLine($"Part 1: {letterFrequency.Values.Max() - letterFrequency.Values.Min()}");
+for (int step = 0; step < 40; step++)
+{
+    var nextStepDict = new Dictionary<string, long>();
+
+    foreach (var rule in input)
+    {
+        var key = $"{rule.FirstChar}{rule.SecondChar}";
+
+        if (!dict.ContainsKey(key)) continue;
+
+        var newKey1 = $"{rule.FirstChar}{rule.InsertedChar}";
+        var newKey2 = $"{rule.InsertedChar}{rule.SecondChar}";
+
+        if (!nextStepDict.ContainsKey(newKey1)) nextStepDict[newKey1] = 0;
+        if (!nextStepDict.ContainsKey(newKey2)) nextStepDict[newKey2] = 0;
+
+        nextStepDict[newKey1] += dict[key];
+        nextStepDict[newKey2] += dict[key];
+
+        if (!letterFrequency.ContainsKey(rule.InsertedChar)) letterFrequency[rule.InsertedChar] = 0;
+        letterFrequency[rule.InsertedChar] += dict[key];
+    }
+
+    dict = nextStepDict;
+    Console.WriteLine($"Step {step + 1}: Letter frequency Max : {letterFrequency.Values.Max()} Min: {letterFrequency.Values.Min()} Diff: {letterFrequency.Values.Max() - letterFrequency.Values.Min()}");
+}
 
 static bool GetInsertionRule(string? input, out InsertionRule? value)
 {
@@ -89,18 +55,13 @@ static bool GetInsertionRule(string? input, out InsertionRule? value)
 
     if (parts.Length != 2) throw new Exception();
 
-    //var repalcementString = parts[0][..1] + parts[1] + parts[0][1..];
-
-    //value = new InsertionRule(parts[0], repalcementString);
-
     var letters = new Regex("[A-Z]").Matches(input).Select(w => w.Value[0]).ToList();
 
     if (letters.Count != 3) throw new Exception();
 
-    value = new InsertionRule(letters[0], letters[1], (char)(letters[2] + 32));
+    value = new InsertionRule(letters[0], letters[1], letters[2]);
 
     return true;
 }
 
-//record InsertionRule(string Base, string ReplacementString);
-record InsertionRule(char FirstChar, char SecondChar, char InsertedChart);
+record InsertionRule(char FirstChar, char SecondChar, char InsertedChar);
