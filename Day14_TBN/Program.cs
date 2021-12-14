036faad50b33ae1394e5fe06f615324906d284ee
David Božjak <bozjak@gmail.com>
2021-12-14 07:36:04 +0100

    A bunch of experimentation, but need to start over for part 2

--- Day14_TBN/Program.cs
+++ Day14_TBN/Program.cs
@@ -9,7 +9,10 @@ var count = input.Count;
 //string polymer = "NNCB";
 string polymer = "OOBFPNOPBHKCCVHOBCSO";
 
-for (int step = 0; step < 40; step++)
+var insertionFrequencyDict = input.Select(w => w.InsertedChart).GroupBy(w => w).ToDictionary(w => w.Key, w => (long)0);
+var ruleDioct = input.ToDictionary(w => w, w => (long)0);
+
+for (int step = 0; step < 10; step++)
 {
     var builder = new StringBuilder(polymer);
 
@@ -27,6 +30,8 @@ for (int step = 0; step < 40; step++)
                 if (builder[i] == insertionRule.SecondChar)
                 {
                     builder.Insert(i, insertionRule.InsertedChart);
+                    insertionFrequencyDict[insertionRule.InsertedChart]++;
+                    ruleDioct[insertionRule]++;
                 }
                 //indexes.Clear();
                 hasFoundFirstIndex = false;
@@ -48,11 +53,31 @@ for (int step = 0; step < 40; step++)
 
     polymer = builder.ToString().ToUpper();
     //Console.WriteLine($"After step {step + 1} length {polymer.Length}: {polymer}");
-    Console.WriteLine($"After step {step + 1} length {polymer.Length}");
+    //Console.WriteLine($"After step {step + 1} length {polymer.Length}");
+
+    var letterFrequency = polymer.ToCharArray().GroupBy(w => w).ToDictionary(w => w.Key, w => w.Count());
+    Console.WriteLine($"Step {step + 1} total length: {polymer.Length} predicted next length: {polymer.Length + polymer.Length - 1}");
+
+    //foreach (var pair in insertionFrequencyDict)
+    //{
+    //    Console.WriteLine($"{pair.Key} : {pair.Value}");
+    //    insertionFrequencyDict[pair.Key] = 0;
+    //}
+
+    foreach (var pair in ruleDioct)
+    {
+        Console.WriteLine($"{pair.Key.FirstChar}{pair.Key.FirstChar} -> {pair.Key.InsertedChart} : {pair.Value}");
+        ruleDioct[pair.Key] = 0;
+    }
+
+    //foreach (var pair in letterFrequency)
+    //{
+    //    Console.WriteLine($"{pair.Key} : {pair.Value} frequency: {Math.Round(pair.Value / (double)polymer.Length * 100, 2)}");
+    //}
 }
 
-var letterFrequency = polymer.ToCharArray().GroupBy(w => w).ToDictionary(w => w.Key, w => w.Count());
-Console.WriteLine($"Part 1: {letterFrequency.Values.Max() - letterFrequency.Values.Min()}");
+//var letterFrequency = polymer.ToCharArray().GroupBy(w => w).ToDictionary(w => w.Key, w => w.Count());
+//Console.WriteLine($"Part 1: {letterFrequency.Values.Max() - letterFrequency.Values.Min()}");
 
 static bool GetInsertionRule(string? input, out InsertionRule? value)
 {
