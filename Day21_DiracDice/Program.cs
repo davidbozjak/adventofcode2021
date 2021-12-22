b46c0bb6cb9ecb4dee89f5663ac85911afd9e7e5
David Božjak <bozjak@gmail.com>
2021-12-22 10:34:22 +0100

    Added day 21 part 2, recursive approach with heavy caching

--- Day21_DiracDice/Program.cs
+++ Day21_DiracDice/Program.cs
@@ -20,6 +20,97 @@ while (players.All(w => w.Score < 1000))
 
 Console.WriteLine($"Part 1: {currentPlayer.Score * die.NumberOfRolls}");
 
+
+Dictionary<(int positionPlayer1, int scorePlayer1, int positionPlayer2, int scorePlayer2, int currentPlayer, int noOfThrowsRemaining), (long winPlayer1, long winPlayer2)> knownStates = new();
+
+var scores = DiracPlay(4, 0, 5, 0, 0, 3);
+
+Console.WriteLine($"Part 2: {scores.winPlayer1} vs {scores.winPlayer2} wins. Max: {Math.Max(scores.winPlayer1, scores.winPlayer2)}");
+
+(long winPlayer1, long winPlayer2) DiracPlay(int positionPlayer1, int scorePlayer1, int positionPlayer2, int scorePlayer2, int currentPlayer, int noOfThrowsRemaining)
+{
+    if (knownStates.ContainsKey((positionPlayer1, scorePlayer1, positionPlayer2, scorePlayer2, currentPlayer, noOfThrowsRemaining)))
+        return knownStates[(positionPlayer1, scorePlayer1, positionPlayer2, scorePlayer2, currentPlayer, noOfThrowsRemaining)];
+
+    long winPlayer1 = 0, winPlayer2 = 0;
+
+    if (noOfThrowsRemaining == 0)
+    {
+        if (currentPlayer == 0)
+        {
+            scorePlayer1 += positionPlayer1;
+
+            if (scorePlayer1 >= 21)
+            {
+                return (1, 0);
+            }
+
+            currentPlayer = 1;
+        }
+        else
+        {
+            scorePlayer2 += positionPlayer2;
+
+            if (scorePlayer2 >= 21)
+            {
+                return (0, 1);
+            }
+
+            currentPlayer = 0;
+        }
+
+        noOfThrowsRemaining = 3;
+    }
+
+    if (currentPlayer == 0)
+    {
+        var positionAfter1 = AdjustPosition(positionPlayer1, 1);
+        var resultAfterThrow1 = DiracPlay(positionAfter1, scorePlayer1, positionPlayer2, scorePlayer2, currentPlayer, noOfThrowsRemaining - 1);
+
+        var positionAfter2 = AdjustPosition(positionPlayer1, 2);
+        var resultAfterThrow2 = DiracPlay(positionAfter2, scorePlayer1, positionPlayer2, scorePlayer2, currentPlayer, noOfThrowsRemaining - 1);
+
+        var positionAfter3 = AdjustPosition(positionPlayer1, 3);
+        var resultAfterThrow3 = DiracPlay(positionAfter3, scorePlayer1, positionPlayer2, scorePlayer2, currentPlayer, noOfThrowsRemaining - 1);
+
+        winPlayer1 = resultAfterThrow1.winPlayer1 + resultAfterThrow2.winPlayer1 + resultAfterThrow3.winPlayer1;
+        winPlayer2 = resultAfterThrow1.winPlayer2 + resultAfterThrow2.winPlayer2 + resultAfterThrow3.winPlayer2;
+
+        knownStates[(positionPlayer1, scorePlayer1, positionPlayer2, scorePlayer2, currentPlayer, noOfThrowsRemaining)] = (winPlayer1, winPlayer2);
+
+        return (winPlayer1, winPlayer2);
+    }
+    else
+    {
+        var positionAfter1 = AdjustPosition(positionPlayer2, 1);
+        var resultAfterThrow1 = DiracPlay(positionPlayer1, scorePlayer1, positionAfter1, scorePlayer2, currentPlayer, noOfThrowsRemaining - 1);
+
+        var positionAfter2 = AdjustPosition(positionPlayer2, 2);
+        var resultAfterThrow2 = DiracPlay(positionPlayer1, scorePlayer1, positionAfter2, scorePlayer2, currentPlayer, noOfThrowsRemaining - 1);
+
+        var positionAfter3 = AdjustPosition(positionPlayer2, 3);
+        var resultAfterThrow3 = DiracPlay(positionPlayer1, scorePlayer1, positionAfter3, scorePlayer2, currentPlayer, noOfThrowsRemaining - 1);
+
+        winPlayer1 = resultAfterThrow1.winPlayer1 + resultAfterThrow2.winPlayer1 + resultAfterThrow3.winPlayer1;
+        winPlayer2 = resultAfterThrow1.winPlayer2 + resultAfterThrow2.winPlayer2 + resultAfterThrow3.winPlayer2;
+
+        knownStates[(positionPlayer1, scorePlayer1, positionPlayer2, scorePlayer2, currentPlayer, noOfThrowsRemaining)] = (winPlayer1, winPlayer2);
+
+        return (winPlayer1, winPlayer2);
+    }
+
+    static int AdjustPosition(int initialPosition, int thrownValue)
+    {
+        var newPosition = initialPosition + thrownValue;
+        while (newPosition > 10)
+        {
+            newPosition -= 10;
+        }
+
+        return newPosition;
+    }
+}
+
 class DeterministicDie
 {
     private int currentValue = 0;
