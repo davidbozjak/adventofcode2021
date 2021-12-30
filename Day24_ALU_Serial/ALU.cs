0234b4d2f2e1de69b73e38e9b05bec9472728f57
David Božjak <bozjak@gmail.com>
2021-12-30 13:45:30 +0100

    Added Day24 and implemented computer

---
+++ Day24_ALU_Serial/ALU.cs
@@ -0,0 +1,90 @@
+﻿class ALU
+{
+    private readonly Instruction[] instructions;
+
+    public ALU(IEnumerable<Instruction> instructions)
+    {
+        this.instructions = instructions.ToArray();
+    }
+
+    public (int w, int x, int y, int z) RunProgramForInput(IEnumerator<int> inputs)
+    {
+        int[] registers = new int[4];
+
+        foreach (var instruction in instructions)
+        {
+            var register1Id = instruction.Register1 - 'w';
+            var register2 = -1;
+            var register2explicitValue = false;
+
+            if (instruction.Register2 != null)
+            {
+                register2explicitValue = int.TryParse(instruction.Register2, out register2);
+
+                if (!register2explicitValue)
+                {
+                    register2 = instruction.Register2[0] - 'w';
+                }
+            }
+
+            switch (instruction.Type)
+            {
+                case InstructionType.Input:
+                    HandleInputInstruction(ref registers[register1Id], inputs);
+                    break;
+                case InstructionType.Add:
+                    HandleAddInstruction(ref registers[register1Id], register2explicitValue ? register2 : registers[register2]);
+                    break;
+                case InstructionType.Multiply:
+                    HandleAddInstruction(ref registers[register1Id], register2explicitValue ? register2 : registers[register2]);
+                    break;
+                case InstructionType.Divide:
+                    HandleDivideInstruction(ref registers[register1Id], register2explicitValue ? register2 : registers[register2]);
+                    break;
+                case InstructionType.Modulo:
+                    HandleModuloInstruction(ref registers[register1Id], register2explicitValue ? register2 : registers[register2]);
+                    break;
+                case InstructionType.Equals:
+                    HandleEqualizeInstruction(ref registers[register1Id], register2explicitValue ? register2 : registers[register2]);
+                    break;
+                default:
+                    throw new Exception();
+            }
+        }
+
+        return (registers[0], registers[1], registers[2], registers[3]);
+    }
+
+    private void HandleInputInstruction(ref int register, IEnumerator<int> inputs)
+    {
+        if (!inputs.MoveNext()) 
+            throw new Exception("Expected more inputs than were provided");
+
+        register = inputs.Current;
+    }
+
+    private void HandleAddInstruction(ref int register, int otherRegister)
+    {
+        register = register + otherRegister;
+    }
+
+    private void HandleMultiplyInstruction(ref int register, int otherRegister)
+    {
+        register = register * otherRegister;
+    }
+
+    private void HandleDivideInstruction(ref int register, int otherRegister)
+    {
+        register = register / otherRegister;
+    }
+
+    private void HandleModuloInstruction(ref int register, int otherRegister)
+    {
+        register = register % otherRegister;
+    }
+
+    private void HandleEqualizeInstruction(ref int register, int otherRegister)
+    {
+        register = register == otherRegister ? 1 : 0;
+    }
+}
---
+++ Day24_ALU_Serial/Program.cs
@@ -0,0 +1,52 @@
+﻿var programLines = new InputProvider<Instruction?>("Input.txt", GetInstruction)
+    .Where(w => w != null).Cast<Instruction>().ToList();
+
+var computer = new ALU(programLines);
+var output = computer.RunProgramForInput(new List<int> { 1, 2, 3 }.GetEnumerator());
+
+Console.WriteLine(programLines.Count);
+
+static bool GetInstruction(string? input, out Instruction? value)
+{
+    value = null;
+
+    if (input == null) return false;
+
+    var parts = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+    var type = parts[0] switch
+    {
+        "inp" => InstructionType.Input,
+        "add" => InstructionType.Add,
+        "mul" => InstructionType.Multiply,
+        "div" => InstructionType.Divide,
+        "mod" => InstructionType.Modulo,
+        "eql" => InstructionType.Equals,
+        _ => throw new Exception()
+    };
+
+    var register1 = parts[1][0];
+    string? register2 = parts.Length == 3 ? parts[2] : null;
+
+    if (type == InstructionType.Input &&
+        register2 != null) throw new Exception();
+
+    if (new[] { InstructionType.Add, InstructionType.Multiply, InstructionType.Divide, InstructionType.Modulo, InstructionType.Equals }.Contains(type) &&
+        register2 == null) throw new Exception();
+
+    value = new Instruction(type, register1, register2);
+
+    return true;
+}
+
+enum InstructionType 
+{ 
+    Input,
+    Add,
+    Multiply,
+    Divide,
+    Modulo,
+    Equals
+}
+
+record Instruction(InstructionType Type, char Register1, string? Register2);
